624ecded741f49ec794f9981905ae95aaa95e2f6
Gabriel <mail@gabrieldavison.me.uk>
2021-10-27 19:25:07 +0100

    #

--- .eleventy.js
+++ .eleventy.js
@@ -1,20 +1,20 @@
-module.exports = function(eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("site/content/**/*.jpeg");
-    eleventyConfig.addPassthroughCopy("site/content/**/*.jpg");
-    eleventyConfig.addPassthroughCopy("site/content/**/*.png");
-    eleventyConfig.addPassthroughCopy("site/content/**/*.webp");
-    eleventyConfig.addPassthroughCopy("site/content/**/*.gif");
-    eleventyConfig.addPassthroughCopy({'site/css': "/"})
-    eleventyConfig.addCollection("postDesc", function(collectionApi) {
-        return collectionApi.getFilteredByTag("post").sort(function(a, b) {
-            return b.date - a.date;
-        });
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("site/content/**/*.jpeg");
+  eleventyConfig.addPassthroughCopy("site/content/**/*.jpg");
+  eleventyConfig.addPassthroughCopy("site/content/**/*.png");
+  eleventyConfig.addPassthroughCopy("site/content/**/*.webp");
+  eleventyConfig.addPassthroughCopy("site/content/**/*.gif");
+  eleventyConfig.addPassthroughCopy({ "site/css": "/" });
+  eleventyConfig.addCollection("postDesc", function (collectionApi) {
+    return collectionApi.getFilteredByTag("post").sort(function (a, b) {
+      return b.date - a.date;
     });
-    return {
-        dir: {
-            input: "./site/content",
-            includes: "./layouts",
-            output: "./site/dist"
-        }
-    };
+  });
+  return {
+    dir: {
+      input: "./site/content",
+      includes: "./layouts",
+      output: "./site/dist",
+    },
+  };
 };
--- app.js
+++ app.js
@@ -1,47 +1,62 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
 const multer = require("multer");
 const upload = multer({ dest: "uploads/" });
-const fs = require('fs');
+const fs = require("fs");
 
 const getTimeStamp = () => {
-    const date = new Date();
-    return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}T${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+  const date = new Date();
+  return `${date.getFullYear()}-${
+    date.getMonth() + 1
+  }-${date.getDate()}T${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
 };
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.sendFile('index.html', {root: __dirname });
+app.get("/", (req, res) => {
+  res.sendFile("index.html", { root: __dirname });
 });
 
-app.post('/', upload.array("files"), async (req, res) => {
-    try {
-        //take file move it into new post folder
-        const postDirName = getTimeStamp();
-        const destinationDir = `${__dirname}/site/posts/${postDirName}`;
-        await fs.promises.mkdir(destinationDir, { recursive: true });
-        for (let file of req.files) {
-            const fileDir = `${__dirname}/uploads/${file.filename}`;
-            await fs.promises.rename(fileDir, `/${destinationDir}/${file.originalname}`);
-        };
-        const imageTags = req.files.reduce((acc, curr) => {
-            return acc + `\n<img loading="lazy" src="/posts/${postDirName}/${curr.originalname}"></img>`;
-        }, "");
-        // const frontMatter = `---\ntags: post\ndate: ${Date.now()}\nreadableDate: ${postDirName}\n---\n`;
-        const frontMatter = `---\ntags: post\ndate: ${postDirName.replace("T", " ")}\n---\n`;
-        const imageBlock = `<div>${imageTags}</div>`;
-        const postBody = `\n\n${req.body.postBody}`;
-        await fs.promises.writeFile(`${destinationDir}/${postDirName}.md`, frontMatter + imageBlock + postBody);
-        res.send('posted');
-    } catch (err) {
-        console.log(err.message);
-        res.send('There was an error', err.message);
+app.post("/", upload.array("files"), async (req, res) => {
+  try {
+    //take file move it into new post folder
+    const postDirName = getTimeStamp();
+    const destinationDir = `${__dirname}/site/content/posts/${postDirName}`;
+    console.log(destinationDir);
+    await fs.promises.mkdir(destinationDir, { recursive: true });
+    for (let file of req.files) {
+      const fileDir = `${__dirname}/uploads/${file.filename}`;
+      await fs.promises.rename(
+        fileDir,
+        `/${destinationDir}/${file.originalname}`
+      );
     }
-    //add some cleanup here for if theres a failure
+    const imageTags = req.files.reduce((acc, curr) => {
+      return (
+        acc +
+        `\n<img loading="lazy" src="/posts/${postDirName}/${curr.originalname}"></img>`
+      );
+    }, "");
+    // const frontMatter = `---\ntags: post\ndate: ${Date.now()}\nreadableDate: ${postDirName}\n---\n`;
+    const frontMatter = `---\ntags: post\ndate: ${postDirName.replace(
+      "T",
+      " "
+    )}\nlayout: post.mustache\n---\n`;
+    const imageBlock = `<div>${imageTags}</div>`;
+    const postBody = `\n\n${req.body.postBody}`;
+    await fs.promises.writeFile(
+      `${destinationDir}/${postDirName}.md`,
+      frontMatter + imageBlock + postBody
+    );
+    res.send("posted");
+  } catch (err) {
+    console.log(err.message);
+    res.send("There was an error", err.message);
+  }
+  //add some cleanup here for if theres a failure
 });
 
 app.listen(port, () => {
-    console.log(`app listening at http://localhost:${port}`);
+  console.log(`app listening at http://localhost:${port}`);
 });
